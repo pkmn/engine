051f3dbd45f3cffd09583990fc305c0d121bd659
Kirk Scheibelhut <kjs@scheibo.com>
2021-11-09 15:26:37 -0800

    Add generate to build script

--- build.zig
+++ build.zig
@@ -12,6 +12,18 @@ pub fn build(b: *std.build.Builder) void {
     const main_tests = b.addTest("src/test.zig");
     main_tests.setBuildMode(mode);
 
-    const test_step = b.step("test", "Run tests");
+    const test_step = b.step("test", "Run all tests");
     test_step.dependOn(&main_tests.step);
+
+    const main_fmts = b.addFmt(&[_][]const u8{"."});
+    const fmt_step = b.step("format", "Run zig fmt on all source files");
+    fmt_step.dependOn(&main_fmts.step);
+
+    const gen_data = b.addSystemCommand(&[_][]const u8{ "node", "tools/generate" });
+    if (b.args) |args| {
+        gen_data.addArgs(args);
+    }
+    const gen_step = b.step("generate", "Generate src/data code");
+    gen_step.dependOn(&gen_data.step);
+    gen_step.dependOn(&main_fmts.step);
 }
