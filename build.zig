1788805c993b0af65475139529ddcb6a3be855c7
Kirk Scheibelhut <kjs@scheibo.com>
2021-11-10 11:19:51 -0800

    Doc generation logic

--- build.zig
+++ build.zig
@@ -6,6 +6,12 @@ pub fn build(b: *std.build.Builder) void {
     const tests = b.addTest("lib/test.zig");
     tests.setBuildMode(mode);
 
+    // FIXME: generated docs are just for zig std lib
+    // const docs = b.addTest("lib/test.zig");
+    // docs.emit_docs = true;
+    // docs.emit_bin = false;
+    // docs.output_dir = "docs";
+
     const format = b.addFmt(&[_][]const u8{"."});
 
     const generate = b.addSystemCommand(&[_][]const u8{ "node", "tools/generate" });
@@ -14,6 +20,7 @@ pub fn build(b: *std.build.Builder) void {
     }
 
     b.step("test", "Run all tests").dependOn(&tests.step);
+    // b.step("docs", "Generate documentation based on source files").dependOn(&docs.step);
     b.step("format", "Run zig fmt on all source files").dependOn(&format.step);
     const g = b.step("generate", "Generate src/data code");
     g.dependOn(&generate.step);
