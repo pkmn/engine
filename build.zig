4425fdd25c9b21b06b5e1c26ba76f7f19be69456
Kirk Scheibelhut <kjs@scheibo.com>
2021-11-10 11:02:26 -0800

    Refactor build.zig

--- build.zig
+++ build.zig
@@ -1,29 +1,21 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    // const lib = b.addStaticLibrary("pkmn", "src/main.zig");
-    // lib.setBuildMode(mode);
-    // lib.install();
+    const tests = b.addTest("lib/test.zig");
+    tests.setBuildMode(mode);
 
-    const main_tests = b.addTest("lib/test.zig");
-    main_tests.setBuildMode(mode);
+    const format = b.addFmt(&[_][]const u8{"."});
 
-    const test_step = b.step("test", "Run all tests");
-    test_step.dependOn(&main_tests.step);
-
-    const main_fmts = b.addFmt(&[_][]const u8{"."});
-    const fmt_step = b.step("format", "Run zig fmt on all source files");
-    fmt_step.dependOn(&main_fmts.step);
-
-    const gen_data = b.addSystemCommand(&[_][]const u8{ "node", "tools/generate" });
+    const generate = b.addSystemCommand(&[_][]const u8{ "node", "tools/generate" });
     if (b.args) |args| {
-        gen_data.addArgs(args);
+        generate.addArgs(args);
     }
-    const gen_step = b.step("generate", "Generate src/data code");
-    gen_step.dependOn(&gen_data.step);
-    gen_step.dependOn(&main_fmts.step);
+
+    b.step("test", "Run all tests").dependOn(&tests.step);
+    b.step("format", "Run zig fmt on all source files").dependOn(&format.step);
+    const g = b.step("generate", "Generate src/data code");
+    g.dependOn(&generate.step);
+    g.dependOn(&format.step);
 }
