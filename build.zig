c3d7d92ee98d3fd44b8fd573d66d52fdb5f9b252
Kirk Scheibelhut <kjs@scheibo.com>
2022-03-01 11:21:32 -0800

    Add build_options.trace

--- build.zig
+++ build.zig
@@ -4,8 +4,10 @@ pub fn build(b: *std.build.Builder) void {
     const mode = b.standardReleaseOptions();
 
     const showdown = b.option(bool, "showdown", "Enable Pokémon Showdown compatability mode") orelse false;
+    const trace = b.option(u16, "trace", "Protocol trace buffer size in bytes (default: 4096)") orelse 4096;
     const build_options = b.addOptions();
     build_options.addOption(bool, "showdown", showdown);
+    build_options.addOption(u16, "trace", trace);
 
     const tests = b.addTest("src/lib/test.zig");
     tests.addOptions("build_options", build_options);
