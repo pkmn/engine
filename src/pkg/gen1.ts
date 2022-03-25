dde76522c7bdf030790c1f63140c4009944b1a3a
Kirk Scheibelhut <kjs@scheibo.com>
2022-03-24 18:34:59 -0700

    Define better serialization types

--- src/pkg/gen1.ts
+++ src/pkg/gen1.ts
@@ -11,16 +11,18 @@ import {
   TypeName,
 } from '@pkmn/data';
 
-import {LE, Lookup, LAYOUT} from './internal';
+import * as I from './index';
+import {LAYOUT, LE, Lookup} from './internal';
 
 const SIZES = LAYOUT[0].sizes;
 const OFFSETS = LAYOUT[0].offsets;
+
 OFFSETS.Battle.p1 = 0;
 OFFSETS.Battle.p2 = SIZES.Side;
 OFFSETS.Stats.spa = OFFSETS.Stats.spd = OFFSETS.Stats.spc;
 OFFSETS.Boosts.spa = OFFSETS.Boosts.spd = OFFSETS.Boosts.spc;
 
-export class Battle {
+export class Battle implements I.Battle {
   static readonly SIZE = 184;
 
   private readonly lookup: Lookup;
@@ -35,7 +37,11 @@ export class Battle {
     this.cache = [undefined, undefined];
   }
 
-  *sides() {
+  get sides() {
+    return this._sides();
+  }
+
+  *_sides() {
     yield this.side('p1');
     yield this.side('p2');
   }
@@ -59,7 +65,11 @@ export class Battle {
     return this.data.getUint16(OFFSETS.Battle.last_damage, LE);
   }
 
-  static encode(
+  get prng(): readonly number[] {
+    return [0, 0, 0, 0]; // TODO
+  }
+
+  static init(
     gen: Generation,
     lookup: Lookup,
     seed: number[],
@@ -68,9 +78,9 @@ export class Battle {
   ) {
     const buf = new ArrayBuffer(SIZES.Battle);
     const data = new DataView(buf);
-    Side.encode(gen, lookup, p1, data, OFFSETS.Battle.p1);
-    Side.encode(gen, lookup, p2, data, OFFSETS.Battle.p2);
-    const offset = OFFSETS.Battle.seed;
+    Side.init(gen, lookup, p1, data, OFFSETS.Battle.p1);
+    Side.init(gen, lookup, p2, data, OFFSETS.Battle.p2);
+    const offset = OFFSETS.Battle.last_damage + 2;
     if (seed.length === 4) {
       data.setUint16(offset + 4, seed[LE ? 3 : 0], LE);
       data.setUint16(offset + 6, seed[LE ? 2 : 1], LE);
@@ -82,11 +92,19 @@ export class Battle {
       }
     }
   }
+
+  static encode(
+    gen: Generation,
+    lookup: Lookup,
+    battle: I.Battle,
+  ) {
+    // TODO
+  }
 }
 
 export type Slot = 1 | 2 | 3 | 4 | 5 | 6;
 
-export class Side {
+export class Side implements I.Side {
   static readonly SIZE = 184;
 
   private readonly lookup: Lookup;
@@ -103,7 +121,11 @@ export class Side {
     this.cache = [undefined, undefined, undefined, undefined, undefined, undefined];
   }
 
-  *pokemon() {
+  get pokemon() {
+    return this._pokemon();
+  }
+
+  *_pokemon() {
     for (let i = 1; i <= 6; i++) {
       yield this.get(i as Slot);
     }
@@ -136,7 +158,7 @@ export class Side {
     return m === 0 ? undefined : this.lookup.moveByNum(m);
   }
 
-  static encode(
+  static init(
     gen: Generation,
     lookup: Lookup,
     team: PokemonSet[],
@@ -146,118 +168,23 @@ export class Side {
     for (let i = 0; i < 6; i++) {
       const poke = team[i];
       const off = offset + OFFSETS.Side.pokemon * i;
-      Pokemon.encode(gen, lookup, poke, data, off);
+      Pokemon.init(gen, lookup, poke, data, off);
       data.setUint8(offset + OFFSETS.Side.order + i, i + 1);
     }
-  }
-}
-
-export class Pokemon {
-  private readonly lookup: Lookup;
-  private readonly data: DataView;
-  private readonly offset: number;
-
-  constructor(lookup: Lookup, data: DataView, offset: number) {
-    this.lookup = lookup;
-    this.data = data;
-    this.offset = offset;
-  }
-
-  stat(stat: StatID): number {
-    return this.data.getUint16(this.offset + OFFSETS.Pokemon.stats + OFFSETS.Stats[stat], LE);
-  }
-
-  get stats(): StatsTable {
-    const stats: Partial<StatsTable> = {};
-    for (const s in OFFSETS.Stats) {
-      if (s === 'spc') continue;
-      const stat = s as StatID;
-      stats[stat] = this.stat(stat);
-    }
-    return stats as StatsTable;
-  }
-
-  move(slot: 1 | 2 | 3 | 4): {move: ID; pp: number} | undefined {
-    const m = this.data.getUint8(this.offset + OFFSETS.Pokemon.moves + (slot - 1) << 1);
-    if (m === 0) return undefined;
-    const move = this.lookup.moveByNum(m);
-    const pp = this.data.getUint8(this.offset + OFFSETS.Pokemon.moves + (slot << 1) - 1);
-    return {move, pp};
-  }
-
-  *moves() {
-    for (let i = 1; i <= 4; i++) {
-      const move = this.move(i as 1 | 2 | 3 | 4);
-      if (!move) break;
-      yield move;
-    }
-  }
-
-  get hp(): number {
-    return this.data.getUint16(this.offset + OFFSETS.Pokemon.hp, LE);
-  }
-
-  get status(): StatusName | undefined {
-    const val = this.data.getUint8(this.offset + OFFSETS.Pokemon.status);
-    if ((val >> 3) & 1) return 'psn';
-    if ((val >> 4) & 1) return 'brn';
-    if ((val >> 5) & 1) return 'frz';
-    if ((val >> 6) & 1) return 'par';
-    return val > 0 ? 'slp' : undefined;
-  }
-
-  get species(): ID {
-    return this.lookup.specieByNum(this.data.getUint8(this.offset + OFFSETS.Pokemon.species));
-  }
-
-  get types(): Readonly<[TypeName] | [TypeName, TypeName]> {
-    return decodeTypes(this.lookup, this.data.getUint8(this.offset + OFFSETS.Pokemon.types));
-  }
-
-  get level(): number {
-    return this.data.getUint8(this.offset + OFFSETS.Pokemon.level);
   }
 
   static encode(
     gen: Generation,
     lookup: Lookup,
-    set: PokemonSet,
+    battle: I.Side,
     data: DataView,
     offset: number
   ) {
-    const species = gen.species.get(set.species)!;
-
-    let hp = 0;
-    let off = offset + OFFSETS.Pokemon.stats;
-    for (const stat of gen.stats) {
-      if (stat === 'spd') break;
-      const val =
-        gen.stats.calc(stat, species.baseStats[stat], set.ivs[stat], set.evs[stat], set.level);
-      data.setUint16(off, val, LE);
-      if (stat === 'hp') hp = val;
-      off += 2;
-    }
-
-    off = offset + OFFSETS.Pokemon.moves;
-    for (const m of set.moves) {
-      const move = gen.moves.get(m)!;
-      data.setUint8(off++, lookup.movesByID(move.id));
-      data.setUint8(off++, move.pp << 2 | 3);
-    }
-
-    data.setUint16(offset + OFFSETS.Pokemon.hp, hp, LE);
-    data.setUint8(offset + OFFSETS.Pokemon.species, lookup.speciesByID(species.id));
-
-    const type1 = species.types[0];
-    const type2 = species.types[1] ?? type1;
-    const types = lookup.typeByName(type1) << 3 | lookup.typeByName(type2);
-    data.setUint8(offset + OFFSETS.Pokemon.types, types);
-
-    data.setUint8(offset + OFFSETS.Pokemon.level, set.level);
+    // TODO
   }
 }
 
-export class ActivePokemon {
+export class ActivePokemon implements I.ActivePokemon {
   static Volatiles = OFFSETS.Volatiles;
 
   private readonly lookup: Lookup;
@@ -290,6 +217,17 @@ export class ActivePokemon {
     return !!(byte & (1 << (bit & 7)));
   }
 
+  get volatiles(): {[name: string]: number} {
+    const volatiles: {[name: string]: number} = {};
+    for (const volatile in ActivePokemon.Volatiles) {
+      if (this.volatile(ActivePokemon.Volatiles[volatile])) {
+        volatiles[volatile] = 0;
+      }
+    }
+    // TODO disabled + data
+    return volatiles;
+  }
+
   move(slot: 1 | 2 | 3 | 4): {move: ID; pp: number; disabled: boolean} | undefined {
     const m = this.data.getUint8(this.offset + OFFSETS.ActivePokemon.moves + (slot - 1) << 1);
     if (m === 0) return undefined;
@@ -300,7 +238,11 @@ export class ActivePokemon {
     return {move, pp, disabled};
   }
 
-  *moves() {
+  get moves() {
+    return this._moves();
+  }
+
+  *_moves() {
     for (let i = 1; i <= 4; i++) {
       const move = this.move(i as 1 | 2 | 3 | 4);
       if (!move) break;
@@ -331,15 +273,140 @@ export class ActivePokemon {
     );
   }
 
-  get types(): Readonly<[TypeName] | [TypeName, TypeName]> {
+  get types(): readonly [TypeName, TypeName] {
     return decodeTypes(this.lookup, this.data.getUint8(this.offset + OFFSETS.ActivePokemon.types));
+  }
+
+  static encode(
+    gen: Generation,
+    lookup: Lookup,
+    battle: I.ActivePokemon,
+    data: DataView,
+    offset: number
+  ) {
+    // TODO
   }
 }
 
-function decodeTypes(lookup: Lookup, val: number): Readonly<[TypeName] | [TypeName, TypeName]> {
-  const type1 = val & 0x0F;
-  const type2 = val >> 4;
-  return (type1 === type2
-    ? [lookup.typeByNum(type1)]
-    : [lookup.typeByNum(type1), lookup.typeByNum(type2)]);
+export class Pokemon implements I.Pokemon {
+  private readonly lookup: Lookup;
+  private readonly data: DataView;
+  private readonly offset: number;
+
+  constructor(lookup: Lookup, data: DataView, offset: number) {
+    this.lookup = lookup;
+    this.data = data;
+    this.offset = offset;
+  }
+
+  stat(stat: StatID): number {
+    return this.data.getUint16(this.offset + OFFSETS.Pokemon.stats + OFFSETS.Stats[stat], LE);
+  }
+
+  get stats(): StatsTable {
+    const stats: Partial<StatsTable> = {};
+    for (const s in OFFSETS.Stats) {
+      if (s === 'spc') continue;
+      const stat = s as StatID;
+      stats[stat] = this.stat(stat);
+    }
+    return stats as StatsTable;
+  }
+
+  move(slot: 1 | 2 | 3 | 4): {move: ID; pp: number} | undefined {
+    const m = this.data.getUint8(this.offset + OFFSETS.Pokemon.moves + (slot - 1) << 1);
+    if (m === 0) return undefined;
+    const move = this.lookup.moveByNum(m);
+    const pp = this.data.getUint8(this.offset + OFFSETS.Pokemon.moves + (slot << 1) - 1);
+    return {move, pp};
+  }
+
+  get moves() {
+    return this._moves();
+  }
+
+  *_moves() {
+    for (let i = 1; i <= 4; i++) {
+      const move = this.move(i as 1 | 2 | 3 | 4);
+      if (!move) break;
+      yield move;
+    }
+  }
+
+  get hp(): number {
+    return this.data.getUint16(this.offset + OFFSETS.Pokemon.hp, LE);
+  }
+
+  get status(): StatusName | undefined {
+    const val = this.data.getUint8(this.offset + OFFSETS.Pokemon.status);
+    if ((val >> 3) & 1) return 'psn';
+    if ((val >> 4) & 1) return 'brn';
+    if ((val >> 5) & 1) return 'frz';
+    if ((val >> 6) & 1) return 'par';
+    return val > 0 ? 'slp' : undefined;
+  }
+
+  get species(): ID {
+    return this.lookup.specieByNum(this.data.getUint8(this.offset + OFFSETS.Pokemon.species));
+  }
+
+  get types(): readonly [TypeName, TypeName] {
+    return decodeTypes(this.lookup, this.data.getUint8(this.offset + OFFSETS.Pokemon.types));
+  }
+
+  get level(): number {
+    return this.data.getUint8(this.offset + OFFSETS.Pokemon.level);
+  }
+
+  static init(
+    gen: Generation,
+    lookup: Lookup,
+    set: PokemonSet,
+    data: DataView,
+    offset: number
+  ) {
+    const species = gen.species.get(set.species)!;
+
+    let hp = 0;
+    let off = offset + OFFSETS.Pokemon.stats;
+    for (const stat of gen.stats) {
+      if (stat === 'spd') break;
+      const val =
+        gen.stats.calc(stat, species.baseStats[stat], set.ivs[stat], set.evs[stat], set.level);
+      data.setUint16(off, val, LE);
+      if (stat === 'hp') hp = val;
+      off += 2;
+    }
+
+    off = offset + OFFSETS.Pokemon.moves;
+    for (const m of set.moves) {
+      const move = gen.moves.get(m)!;
+      data.setUint8(off++, lookup.movesByID(move.id));
+      data.setUint8(off++, move.pp);
+    }
+
+    data.setUint16(offset + OFFSETS.Pokemon.hp, hp, LE);
+    data.setUint8(offset + OFFSETS.Pokemon.species, lookup.speciesByID(species.id));
+
+    const type1 = species.types[0];
+    const type2 = species.types[1] ?? type1;
+    const types = lookup.typeByName(type1) << 3 | lookup.typeByName(type2);
+    data.setUint8(offset + OFFSETS.Pokemon.types, types);
+
+    data.setUint8(offset + OFFSETS.Pokemon.level, set.level);
+  }
+
+  static encode(
+    gen: Generation,
+    lookup: Lookup,
+    battle: I.Pokemon,
+    data: DataView,
+    offset: number
+  ) {
+    // TODO
+  }
+}
+
+function decodeTypes(lookup: Lookup, val: number): readonly [TypeName, TypeName] {
+  return [lookup.typeByNum(val & 0x0F), lookup.typeByNum(val >> 4)];
 }
--- src/pkg/index.ts
+++ src/pkg/index.ts
@@ -1,10 +1,67 @@
-import {Generation} from '@pkmn/data';
+import {
+  BoostsTable,
+  Generation,
+  ID,
+  StatsTable,
+  StatusName,
+  TypeName,
+} from '@pkmn/data';
 
 import {Lookup} from './internal';
 import * as gen1 from './gen1';
 
+export interface Battle extends Gen1.Battle {}
+export interface Side extends Gen1.Side {}
+export interface ActivePokemon extends Gen1.ActivePokemon {}
+export interface Pokemon extends Gen1.Pokemon {}
+export interface MoveSlot extends Gen1.MoveSlot {}
+
+export namespace Gen1 {
+  export interface Battle {
+    sides: Iterable<Side>;
+    turn: number;
+    lastDamage: number;
+    prng: readonly number[];
+  }
+
+  export interface Side {
+    active: ActivePokemon;
+    pokemon: Iterable<Pokemon>;
+    lastUsedMove: ID | undefined;
+    lastSelectedMove: ID | undefined;
+  }
+
+  export interface ActivePokemon {
+    species: ID;
+    types: readonly [TypeName, TypeName];
+    stats: StatsTable;
+    boosts: BoostsTable;
+    volatiles: {[name: string]: number};
+    moves: Iterable<MoveSlot>;
+  }
+
+  export interface Pokemon {
+    species: ID;
+    types: readonly [TypeName, TypeName];
+    hp: number;
+    status: StatusName | undefined;
+    stats: StatsTable;
+    level: number;
+    moves: Iterable<Omit<MoveSlot, 'disabled'>>;
+  }
+
+  export interface MoveSlot {
+    move: ID;
+    pp: number;
+    disabled: boolean;
+  }
+}
+
 export class Battle {
   static create(gen: Generation, buf: ArrayBuffer) {
-    return new gen1.Battle(Lookup.get(gen), new DataView(buf));
+    switch (gen.num) {
+    case 1: return new gen1.Battle(Lookup.get(gen), new DataView(buf));
+    default: throw new Error(`Unsupported gen ${gen.num}`);
+    }
   }
 }
