daff7f760473faa51d591b2d0033ca790a8271b4
Kirk Scheibelhut <kjs@scheibo.com>
2022-03-30 17:24:09 -0700

    Set up zig coverage

--- build.zig
+++ build.zig
@@ -6,7 +6,8 @@ const Pkg = std.build.Pkg;
 pub fn build(b: *Builder) !void {
     const mode = b.standardReleaseOptions();
 
-    const showdown = b.option(bool, "showdown", "Enable Pokémon Showdown compatability mode") orelse false;
+    const showdown =
+        b.option(bool, "showdown", "Enable Pokémon Showdown compatability mode") orelse false;
     const strip = b.option(bool, "strip", "Strip debugging symbols from binary") orelse false;
     const trace = b.option(bool, "trace", "Enable trace logs") orelse false;
 
@@ -27,10 +28,13 @@ pub fn build(b: *Builder) !void {
     lib.setBuildMode(mode);
     lib.install();
 
-    const test_file = b.option([]const u8, "test-file", "Input file for test") orelse "src/lib/test.zig";
+    const coverage = b.option(bool, "test-coverage", "Generate test coverage") orelse false;
+    const test_file =
+        b.option([]const u8, "test-file", "Input file for test") orelse "src/lib/test.zig";
     const test_bin = b.option([]const u8, "test-bin", "Emit test binary to");
     const test_filter = b.option([]const u8, "test-filter", "Skip tests that do not match filter");
-    const test_no_exec = b.option(bool, "test-no-exec", "Compiles test binary without running it") orelse false;
+    const test_no_exec =
+        b.option(bool, "test-no-exec", "Compiles test binary without running it") orelse false;
 
     const tests = if (test_no_exec) b.addTestExe("test_exe", test_file) else b.addTest(test_file);
     tests.setMainPkgPath("./");
@@ -42,6 +46,14 @@ pub fn build(b: *Builder) !void {
     if (test_bin) |bin| {
         tests.name = std.fs.path.basename(bin);
         if (std.fs.path.dirname(bin)) |dir| tests.setOutputDir(dir);
+    }
+    if (coverage) {
+        tests.setExecCmd(&[_]?[]const u8{
+            "kcov",
+            "--include-pattern=src/lib",
+            "coverage/kcov",
+            null,
+        });
     }
 
     const format = b.addFmt(&[_][]const u8{"."});
