9ed6f2c4b26913fbf358d82b90bd661c9c19d504
Kirk Scheibelhut <kjs@scheibo.com>
2022-04-07 19:55:41 -0700

    figure out initial types

--- src/tools/simulate.zig
+++ src/tools/simulate.zig
@@ -1,23 +1,12 @@
 const std = @import("std");
 const pkmn = @import("pkmn");
 
-/// .pkmn.gen1.Pokemon{
-///    .species = .Moltres,
-///    .types = .{ .type1 = .Fire, .type2 = .Flying },
-///    .stats = .{ .hp = 323, .atk = 238, .def = 217, .spe = 218, .spc = 288 },
-///    .moves = {
-///        .{ .id = .Flamethrower, .pp = 24 },
-///        .{ .id = .Scratch, .pp = 56 },
-///        .{ .id = .SwordsDance, .pp = 48 },
-///        .{ .id = .Clamp, .pp = 16 }
-///    },
-///    .hp = 323,
-///    .status = 16,
-///}
+// https://gist.github.com/scheibo/ae7bef7600d0a2af508c6d03e419e0b8
 pub fn simulate(gen: u8, num: usize, seed: u64) !void {
     std.debug.assert(gen >= 1 and gen <= 8);
 
     var random = pkmn.rng.Random.init(seed);
+    // var random = std.rand.DefaultPrng.init(seed).random();
 
     var options: [pkmn.MAX_OPTIONS_SIZE]pkmn.Choice = undefined;
 
@@ -33,8 +22,10 @@ pub fn simulate(gen: u8, num: usize, seed: u64) !void {
 
         var result = try battle.update(c1, c2, null);
         while (result.type == .None) : (result = try battle.update(c1, c2, null)) {
-            c1 = options[random.range(0, battle.choices(.P1, result.p1, options))];
-            c2 = options[random.range(0, battle.choices(.P2, result.p2, options))];
+            c1 = options[random.range(0, battle.choices(.P1, result.p1, options) - 1)];
+            c2 = options[random.range(0, battle.choices(.P2, result.p2, options) - 1)];
+            // c1 = options[random.uintLessThan(battle.choices(.P1, result.p1, options))];
+            // c2 = options[random.uintLessThan(battle.choices(.P2, result.p2, options))];
         }
 
         const msg = switch (result.type) {
