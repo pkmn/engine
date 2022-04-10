7893a23c3eb30d222a29f6d10882ab9ea25d601b
Kirk Scheibelhut <kjs@scheibo.com>
2022-04-09 21:33:08 -0700

    Add team importables to example/js

--- src/examples/js/example.js
+++ src/examples/js/example.js
@@ -1,5 +1,14 @@
-import {Generations} from "@pkmn/data";
-import {Dex} from "@pkmn/dex";
+const fs = require('fs');
+
+const {Generations} = require("@pkmn/data");
+const {Dex} = require("@pkmn/dex");
+const {Team} = require("@pkmn/sets");
+
+const TEAMS = {
+  p1: Team.import(fs.readFileSync(__dirname + '/teams/p1.txt', 'utf-8'), Dex).team,
+  p2: Team.import(fs.readFileSync(__dirname + '/teams/p2.txt', 'utf-8'), Dex).team,
+};
 
 const gens = new Generations(Dex);
 console.log(gens.get(1).species.get('Gengar').baseStats.hp);
+console.log(TEAMS);
