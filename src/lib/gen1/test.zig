30c51d10e104fec2bdf72f95d7cce879a0c6fa54
Kirk Scheibelhut <kjs@scheibo.com>
2022-05-28 22:27:45 -0700

    more todos

--- src/lib/gen1/test.zig
+++ src/lib/gen1/test.zig
@@ -353,7 +353,6 @@ test "turn order (switch vs. move)" {
 }
 
 test "PP deduction" {
-
     return error.SkipZigTest;
 }
 
@@ -1171,6 +1170,8 @@ test "Transform" {
     // The user transforms into the target. The target's current stats, stat stages, types, moves,
     // DVs, species, and sprite are copied. The user's level and HP remain the same and each copied
     // move receives only 5 PP. This move can hit a target using Dig or Fly.
+
+    // TODO make sure don't decrement stored PP
     return error.SkipZigTest;
 }
 
