daf3929ec1e135c4ab97fb2094feb8fc9cb9b8b9
Kirk Scheibelhut <kjs@scheibo.com>
2023-01-20 12:16:18 -0800

    set default cycles based on duration

--- src/test/integration/index.ts
+++ src/test/integration/index.ts
@@ -8,11 +8,12 @@ import {run} from './common';
 
 (async () => {
   const gens = new Generations(Dex as any);
-  const argv = minimist(process.argv.slice(2), {default: {maxFailures: 5, cycles: 10}});
+  const argv = minimist(process.argv.slice(2), {default: {maxFailures: 5}});
   const unit =
     typeof argv.duration === 'string' ? argv.duration[argv.duration.length - 1] : undefined;
   const duration =
     unit ? +argv.duration.slice(0, -1) * {s: 1e3, m: 6e4, h: 3.6e6}[unit]! : argv.duration;
+  argv.cycles = argv.cycles ?? duration ? 1 : 10;
   const seed = argv.seed ? argv.seed.split(',').map((s: string) => Number(s)) : null;
   await run(gens, {prng: new PRNG(seed), log: process.stdout.isTTY, ...argv, duration});
 })().catch(err => {
