5a379d11ec4f93a4d1e1cd5a2418b3d1227854cb
Kirk Scheibelhut <kjs@scheibo.com>
2023-03-04 20:09:17 -0800

    rework configuration for integration test

--- src/test/integration/index.test.ts
+++ src/test/integration/index.test.ts
@@ -4,6 +4,7 @@ import {Dex} from '@pkmn/sim';
 import {run} from './common';
 
 describe('integration', () => {
+  // TODO: skip if not -Dshowdown and -Dtrace!
   it('test', async () => {
     const gens = new Generations(Dex as any);
     expect(await run(gens, {prng: [1, 2, 3, 4]})).toBe(0);
