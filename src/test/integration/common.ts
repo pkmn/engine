8db218fe886716be289991be7457479540338714
Kirk Scheibelhut <kjs@scheibo.com>
2023-03-07 15:12:20 -0800

    begin making integration test work

--- src/test/integration/common.ts
+++ src/test/integration/common.ts
@@ -1,29 +1,30 @@
-// TODO: actually use @pkmn/engine
-// - feed input from rawBattleStream.rawInputLog into @pkmn/engine & compare
-// - add buf to output if doesn't compare
-// - verify binary protocol round trip
-// - handle invalid choices by RandomPlayerAI
+import {strict as assert} from 'assert';
 
 import {Generations, Generation, GenerationNum} from '@pkmn/data';
-import {PRNG, PRNGSeed, BattleStreams, ID} from '@pkmn/sim';
+import {Protocol} from '@pkmn/protocol';
+import {PRNG, PRNGSeed, BattleStreams, ID, State} from '@pkmn/sim';
 import {
   AIOptions, ExhaustiveRunner, ExhaustiveRunnerOptions,
   ExhaustiveRunnerPossibilites, RunnerOptions,
 } from '@pkmn/sim/tools';
 
+import * as engine from '../../pkg';
 import {PatchedBattleStream, patch} from '../showdown/common';
 
 import blocklistJSON from '../blocklist.json';
 
+const FORMATS = ['gen1customgame'];
 const BLOCKLIST = blocklistJSON as {[gen: number]: Partial<ExhaustiveRunnerPossibilites>};
 
 class Runner {
+  private readonly gen: Generation;
   private readonly format: string;
   private readonly prng: PRNG;
   private readonly p1options?: AIOptions;
   private readonly p2options?: AIOptions;
 
-  constructor(options: RunnerOptions) {
+  constructor(gen: Generation, options: RunnerOptions) {
+    this.gen = gen;
     this.format = options.format;
 
     this.prng = (options.prng && !Array.isArray(options.prng))
@@ -48,18 +49,57 @@ class Runner {
       seed: this.newSeed(), move: 0.7, mega: 0.6, ...this.p2options!,
     }).start();
 
-    await Promise.all([streams.omniscient.write(
+    const start = streams.omniscient.write(
       `>start ${JSON.stringify(spec)}\n` +
       `>player p1 ${JSON.stringify(p1spec)}\n` +
       `>player p2 ${JSON.stringify(p2spec)}`
-    ), p1, p2]);
+    );
 
     const buf = [];
     for await (const chunk of streams.omniscient) {
       buf.push(chunk);
     }
 
-    // BUG: streams.p2.writeEnd ?
+    await Promise.all([streams.p2.writeEnd(), p1, p2, start]);
+
+    const options = {
+      p1: {name: 'Bot 1', team: this.p1options!.team!},
+      p2: {name: 'Bot 2', team: this.p1options!.team!},
+      seed: spec.seed,
+      showdown: true,
+      log: true,
+    };
+    const battle = engine.Battle.create(this.gen, options);
+    const log = new engine.Log(this.gen, engine.Lookup.get(this.gen), options);
+
+    let c1 = engine.Choice.pass();
+    let c2 = engine.Choice.pass();
+
+    let input = 0;
+
+    let result: engine.Result = {type: undefined, p1: 'pass', p2: 'pass'};
+    for (const chunk of buf) {
+      assert.equal(result.type, undefined);
+      result = battle.update(c1, c2);
+      if (result.type === 'win') {
+        assert.equal(rawBattleStream.battle!.winner, options.p1.name);
+      } else if (result.type === 'lose') {
+        assert.equal(rawBattleStream.battle!.winner, options.p2.name);
+      } else if (result.type === 'tie') {
+        assert.equal(rawBattleStream.battle!.winner, '');
+      } else if (result.type) {
+        throw new Error('Battle ended in error which should be impossible with -Dshowdown');
+      } else {
+        assert.deepStrictEqual(
+          Array.from(Protocol.parse(normalize(chunk))),
+          Array.from(log.parse(battle.log!)),
+        );
+      }
+      [c1, c2, input] = nextChoices(battle, result, rawBattleStream.rawInputLog, input);
+    }
+
+    assert.equal(rawBattleStream.rawInputLog.length, input);
+    assert.notEqual(result.type, undefined);
   }
 
   // Same as PRNG#generatedSeed, only deterministic.
@@ -90,17 +130,38 @@ class RawBattleStream extends PatchedBattleStream {
   }
 }
 
-const FORMATS = [
-  'gen1customgame',
-  'gen2customgame',
-  // 'gen3customgame', 'gen3doublescustomgame',
-  // 'gen4customgame', 'gen4doublescustomgame',
-  // 'gen5customgame', 'gen5doublescustomgame',
-  // 'gen6customgame', 'gen6doublescustomgame',
-  // 'gen7customgame', 'gen7doublescustomgame',
-  // 'gen8customgame', 'gen8doublescustomgame',
-  // 'gen9customgame', 'gen9doublescustomgame',
-];
+function normalize(chunk: string) {
+  return State.normalizeLog(chunk) as string; // TODO
+}
+
+const SKIP = /^>(version|start|player)/;
+const MATCH = /^>(p1|p2) (?:(pass)|((move) ([1-4]))|((switch) ([2-6])))/;
+
+function nextChoices(battle: engine.Battle, result: engine.Result, input: string[], index: number) {
+  // let c1: engine.Choice;
+  // let c2: engine.Choice;
+
+  while (index < input.length) {
+    const m = MATCH.exec(input[index]);
+    if (!m) {
+      if (!SKIP.test(input[index])) {
+        throw new Error(`Unexpected input ${index}: '${input[index]}'`);
+      } else {
+        continue;
+      }
+    }
+    const player = m[1] as engine.Player;
+    const type = (m[2] ?? m[4] ?? m[7]) as engine.Choice['type'];
+    const data = +(m[5] ?? m[8] ?? 0);
+
+    index++;
+  }
+
+  const c1 = engine.Choice.pass(); // TODO
+  const c2 = engine.Choice.pass(); // TODO
+
+  return [c1, c2, index] as const;
+}
 
 function possibilities(gen: Generation) {
   const blocked = BLOCKLIST[gen.num] || {};
@@ -129,8 +190,7 @@ type Options = Pick<ExhaustiveRunnerOptions, 'log' | 'maxFailures' | 'cycles'> &
 
 export async function run(gens: Generations, options: Options) {
   const opts: ExhaustiveRunnerOptions = {
-    cycles: 1, maxFailures: 1, log: false, ...options,
-    runner: o => new Runner(o).run(), format: '',
+    cycles: 1, maxFailures: 1, log: false, ...options, format: '',
   };
 
   let failures = 0;
@@ -142,7 +202,8 @@ export async function run(gens: Generations, options: Options) {
       patch.generation(gen);
       opts.format = format;
       opts.possible = possibilities(gen);
-      failures += await (new ExhaustiveRunner(opts).run());
+      failures +=
+        await (new ExhaustiveRunner({...opts, runner: o => new Runner(gen, o).run()}).run());
       if (opts.log) process.stdout.write('\n');
       if (failures >= opts.maxFailures!) return failures;
     }
