8af18c0ddf33790d69c8ec8022acb823162a5b95
Kirk Scheibelhut <kjs@scheibo.com>
2023-05-18 10:58:39 -0700

    add -Dchance everywhere

--- src/test/fuzz.ts
+++ src/test/fuzz.ts
@@ -28,7 +28,7 @@ export async function run(
   seed?: bigint,
   testing?: boolean,
 ) {
-  const args = ['build', '-j1', '--summary', 'failures', 'fuzz', '-Dlog'];
+  const args = ['build', 'j1', '--summary', 'failures', 'fuzz', '-Dlog', '-Dchance'];
   if (showdown) args.push('-Dshowdown');
   args.push('--', gen.toString(), duration);
   if (seed) args.push(seed.toString());
