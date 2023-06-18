08af89fd7bd143c4bf3d54a74e624923a0838257
Kirk Scheibelhut <kjs@scheibo.com>
2023-06-18 11:40:52 -0700

    fix lint length

--- src/lib/common/battle.zig
+++ src/lib/common/battle.zig
@@ -1,8 +1,8 @@
 /// Stores state required by optional features to the engine that can be passed to a generation's
 /// `Battle.update` function. All generations generically support protocol logging (enabled via
-/// `-Dlog`), chance actions and probability tracking (enabled via `-Dchance`), and damage computation
-/// and chance overrides (`-Dcalc`), though the concrete types for these features are generation
-/// dependent.
+/// `-Dlog`), chance actions and probability tracking (enabled via `-Dchance`), and damage
+/// computation and chance overrides (`-Dcalc`), though the concrete types for these features are
+/// generation dependent.
 pub fn Options(comptime Log: type, comptime Chance: type, comptime Calc: type) type {
     return struct { log: Log, chance: Chance, calc: Calc };
 }
